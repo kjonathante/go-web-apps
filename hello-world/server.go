9888aa75c5fe7a83be75742e9091feaa67392de1
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-27 21:51:47 -0700

    Added a hello world webserver.

---
+++ hello-world/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	http.HandleFunc("/", func (res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(res, "Hello, you've requested: %s\n", req.URL.Path)
+	})
+
+	helloHandler := func(res http.ResponseWriter, req *http.Request) {
+		io.WriteString(res, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
